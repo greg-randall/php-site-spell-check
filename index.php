704a140ad70ca5a9645fa4581e2d8ab314e891de
Greg Randall <greg.randall@ua.edu>
2017-05-12 16:32:37 -0500

    before link rewriter

--- index.php
+++ index.php
@@ -1,25 +1,23 @@
 <?php
 include 'dict.php'; // array called $dictionary.
 
-$ignore = array(
-		
-);
+$url=$_GET["p"];
 
-
-$page = file_get_contents ("https://gobama.ua.edu/steps/transfer/");
+$page = file_get_contents ($url);
 
 $page=preg_replace( "/\r|\n/", "", $page);
 $page=preg_replace( "/\s+/", " ", $page);
 $page=str_replace(array(">","<"),array(">\n","\n<"),$page);
-$page=preg_replace( "/>[\s\n\r]+</", "><", $page);
+//$page=preg_replace( "/>[\s\n\r]+</", "><", $page);
 $page=str_replace("\n ","\n",$page);
-
-//echo $page;
-
 
 $lines=explode("\n",$page);
 
 foreach ($lines as &$line) {
+	
+	//if(substr($line,0,2)=="<a"){
+	//	echo "link";
+	//}
 	
 	if($line[0]!="<"&&substr($line,0,3)!="-->"){
 		$words=explode(" ",$line);
@@ -34,20 +32,18 @@ foreach ($lines as &$line) {
 		
 	}else{
 		echo "\n".$line."\n";
-	}
-	/*	*/
-				
+	}			
 }
 
 
 function clean_word($word){
 	
 	$clean_word=strtoupper($word); //make string uppercase
-	$clean_word=trim($clean_word);
-	if(!ctype_alnum(substr($clean_word,-2,1))){
+	$clean_word=trim($clean_word); //remove leading/trailing whitespace
+	if(!ctype_alnum(substr($clean_word,-2,1))&&substr($clean_word,-1,1)=="S"){//if word has apostophie and a 's' (ie university's) drop those charecters
 		$clean_word=substr($clean_word,0,-2);			
 	}
-	$clean_word=html_entity_decode($clean_word);
+	$clean_word=html_entity_decode($clean_word); //decode the html encoded stuff to regular letters (ie &nbsp; to " ")
 	$clean_word=preg_replace("/[^\w]/", '', $clean_word); //remove all non letters
 	$clean_word=preg_replace("/[0-9]+/", '', $clean_word); //remove all non letters
 	return($clean_word);
